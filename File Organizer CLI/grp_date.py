b0f468c425650d89cd4552a5185ae49b4fd4d987
Aadi-RE350 <bansodeaditya0@gmail.com>
2026-03-08 03:44:18 +0530

    added summary

--- File Organizer CLI/grp_date.py
+++ File Organizer CLI/grp_date.py
@@ -30,7 +30,7 @@ def get_date(file: Path) -> tuple[str, str]: #year,month
     return str(dt_object.year),dt_object.strftime('%b')
 
 
-def process(file:Path,dirpath:Path,moves:list,isdry:bool):
+def process_file(file:Path,dirpath:Path,moves:list,is_dry:bool)->tuple[int,int]:
     year,month = get_date(file)
 
     des_dir = dirpath / year / month
@@ -38,9 +38,9 @@ def process(file:Path,dirpath:Path,moves:list,isdry:bool):
 
     if des_path.exists():
         print(f"Skipping existing file: {des_path}")
-        return
+        return 0,1
     
-    if not isdry:
+    if not is_dry:
         moves.append({
             "src": str(file),
             "des": str(des_path)
@@ -49,13 +49,14 @@ def process(file:Path,dirpath:Path,moves:list,isdry:bool):
         shutil.copy2(file,des_path) #only for testing
     else:
         print(f"[DRY RUN] {file} -> {des_path}")
+    return 1,0
 
 
 def save_moves(logfile:Path,moves:list):
     with open(logfile,'w') as f:
         json.dump(moves,f,indent=4)
 
-def by_date(dirpath:Path,isdry:bool):
+def by_date(dirpath:Path,is_dry:bool):
     if not dirpath.is_dir():
         raise ValueError('Argument provided must be a directory')
     
@@ -63,20 +64,22 @@ def by_date(dirpath:Path,isdry:bool):
     logfile = dirpath / 'moves.json'
     moves = load_moves(logfile)
 
+    skipped = 0
+    moved = 0
+
     # process 
     for file in dirpath.iterdir():
-        # only files or not a symlink
-        if not file.is_file() or file.is_symlink():
+        if not file.is_file() or file.name in IGNORE_FILES or file.is_symlink():
             continue
-        # ignore code files
-        if file.name in IGNORE_FILES:
-            continue
-        
 
-        process(file,dirpath,moves,isdry)
+        m,s = process_file(file,dirpath,moves,is_dry)
+        moved += m
+        skipped += s
     
-    if not isdry:
+    if not is_dry:
         save_moves(logfile,moves)
+    
+    print(f'\nSummary : {moved} files moved, {skipped} files skipped\n')
 
 
 
--- File Organizer CLI/grp_ext.py
+++ File Organizer CLI/grp_ext.py
@@ -27,23 +27,24 @@ def get_category(file:Path)->str:
         category = 'Unknown'
     return category
 
-def process_file(file:Path,dirpath:Path,is_dry:bool,moves:list):
+def process_file(file:Path,dirpath:Path,moves:list,is_dry:bool)->tuple[int,int]:
     category = get_category(file)
     des_dir = dirpath / category
     des_path = des_dir / file.name
     if des_path.exists():
         print(f"Skipping existing file: {des_path}")
-        return
+        return 0,1
 
     if not is_dry:
         moves.append({
             "src": str(file),
             "des": str(des_path)
         })
-        des_dir.mkdir(exist_ok=True)
+        des_dir.mkdir(parents=True,exist_ok=True)
         shutil.copy2(file,des_path)
     else:
         print(f"[DRY RUN] {file} -> {des_path}")
+    return 1,0
 
 def save_moves(log_file:Path,moves:list):
     with open(log_file,'w') as f:
@@ -56,20 +57,21 @@ def by_ext(dirpath:Path,is_dry:bool):
     log_file = dirpath / 'moves.json'
     moves = load_moves(log_file)
 
+    skipped = 0
+    moved = 0
+
     for file in dirpath.iterdir():
-        if not file.is_file():
+        if not file.is_file() or file.name in IGNORE_FILES or file.is_symlink():
             continue
 
-        if file.name in IGNORE_FILES:
-            continue
+        m,s = process_file(file,dirpath,moves,is_dry)
+        moved += m
+        skipped += s
 
-        if file.parent != dirpath:
-            continue
-
-        process_file(file,dirpath,is_dry,moves)
-    
     if not is_dry:
         save_moves(log_file,moves)
+
+    print(f'\nSummary : {moved} files moved, {skipped} files skipped\n')
 
 
 if __name__ == "__main__":
